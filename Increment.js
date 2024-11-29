f7b1b98276c14c5fa09b9f00fb9861bd581cfbfc
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-11-30 01:11:53 +0600

    JSX and css added

--- Increment.js
+++ Increment.js
@@ -1,4 +1,23 @@
 const domContainer = document.querySelector("#root");
 
+// const myElement = React.createElement("div", null,
+//     [   
+//         React.createElement("h1", null, "Hello world!"),
+//         React.createElement("h1", null, "Hello world!")
+//     ]);
 
-ReactDOM.render("Hello", domContainer);
+//JSX
+
+const myElement = (
+    <div>
+        <h1 id="display">0</h1>
+        <div>
+            <button id ="button">
+                Increment +
+            </button>
+        </div>
+    </div>
+)
+
+
+ReactDOM.render(myElement, domContainer);
