98bfd1de0dc335272e8886ca4c4abd76e4e319a8
Tahmid Muntaser <tahmid25muntaser@gmail.com>
2024-11-30 18:45:43 +0600

    dom:

---
+++ dom/dom.js
@@ -0,0 +1,22 @@
+let array = [];
+cnt = 0;
+
+let container = document.querySelector('.container');
+
+// 1 time dom operation
+
+while(cnt<10000){
+    array.push(++cnt);
+}
+
+container.innerHTML = array.join(' ');
+
+
+
+
+// 10000 times dom operation
+// while(cnt<10000){
+//     cnt++;
+
+//     container.innerHTML += ` ${cnt}`;
+// }
